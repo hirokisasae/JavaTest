4fa04127cef29dfe06a67aa22aab2efc56e5d3a7
h.sasae <h.sasae@WSAMZN-MCM9VP9I.corp.amazonworkspaces.com>
2021-05-14 10:01:02 +0900

    Revert "Javaテスト"
    This reverts commit e4591b09fa3446dcaeee6e3c8b90121b7451c15f.

--- src/jp/co/aforce/test/Airplane.java
+++
@@ -1,9 +0,0 @@
-package jp.co.aforce.test;
-
-public class Airplane {
-	
-	public void fly() {
-		System.out.println("飛行します");
-	}
-
-}
--- src/jp/co/aforce/test/Fighter.java
+++
@@ -1,9 +0,0 @@
-package jp.co.aforce.test;
-
-public class Fighter extends Airplane {
-
-	public void fight() {
-		System.out.println("戦闘します");
-	}
-
-}
